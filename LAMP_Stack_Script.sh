67ccbc52a7f2da4ee6c1bb315f2deb478fd31997
Varun Upadhyay <varun.u28@gmail.com>
2018-04-04 19:24:46 -0700

    Update LAMP_Stack_Script.sh

--- LAMP_Stack_Script.sh
+++ LAMP_Stack_Script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#The script creates an index.html which displays the instance id
 sudo su
 yum update -y
 yum groupinstall -y "Web Server" 
@@ -12,3 +13,5 @@ chown -R root:www /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} +
 find /var/www/ -type f -exec chmod 0664 {} +
+val=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+echo "<h1>Instance id is: $val</h1>" > index.html
